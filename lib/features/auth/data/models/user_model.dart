5114bab8de48faff1b10680417aa63a2800cc540
gauss2302 <shilov6865@gmail.com>
2024-04-11 22:46:09 +0500

    Add UserModel class for authentication feature

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,31 @@
+class UserModel {
+  final String id;
+  final String email;
+  final String name;
+  final String photoUrl;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.photoUrl,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      email: json['email'],
+      name: json['name'],
+      photoUrl: json['photoUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+      'photoUrl': photoUrl,
+    };
+  }
+}
---
+++ lib/features/auth/domain/entities/user_entities.dart
@@ -0,0 +1,15 @@
+class UserEntities {
+  final String id;
+  final String username;
+  final String email;
+  final String name;
+  final String photoUrl;
+
+  UserEntities({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.name,
+    required this.photoUrl,
+  });
+}
