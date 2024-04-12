4fc93612a888bc15d199634e233c600b2be8b3b3
gauss2302 <shilov6865@gmail.com>
2024-04-12 16:54:31 +0500

    Goals Screen basic set-up

--- lib/features/goals/presentation/pages/goals_screen.dart
+++ lib/features/goals/presentation/pages/goals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nail_it/core/widgets/appbar.dart';
 import 'package:nail_it/core/widgets/appbar.dart';
 import 'package:nail_it/core/widgets/drawer.dart';
@@ -14,14 +15,58 @@ class GoalsScreen extends StatefulWidget {
 class _GoalsScreenState extends State<GoalsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: UpperBar(
         appbartitle: 'M Y  G O A L S',
       ),
       drawer: DrawerMenu(),
       bottomNavigationBar: NavBar(),
       body: Center(
-        child: Text('Goals Screen'),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'My Goals',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              RichText(
+                text: const TextSpan(
+                  text:
+                      'This is where you can set your goals and track your progress. You can also see your achievements here.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+              // Add Database fetch here, create milestones for achievements
+              SizedBox(
+                height: 200, // or any other value
+                child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 5,
+                    childAspectRatio: 1,
+                  ),
+                  children: const <Widget>[
+                    Icon(Icons.star),
+                    Icon(Icons.favorite),
+                    Icon(Icons.check),
+                    Icon(Icons.done_all_sharp),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
