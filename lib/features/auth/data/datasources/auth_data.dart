5aefc0c46ffdac59b3677afc950eadd2feba355a
gauss2302 <shilov6865@gmail.com>
2024-04-14 01:22:25 +0500

    Implement signUpWithEmailPassword method in AuthDataSourceImpl

--- lib/features/auth/data/datasources/auth_data.dart
+++ lib/features/auth/data/datasources/auth_data.dart
@@ -1,4 +1,6 @@
+import 'package:nail_it/core/error/exception.dart';
 import 'package:nail_it/features/auth/data/models/user_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthDataSource {
   Future<UserModel> signUpWithEmailPassword({
@@ -16,16 +18,18 @@ abstract interface class AuthDataSource {
 }
 
 class AuthDataSourceImpl implements AuthDataSource {
+  final SupabaseClient superbaseClient;
+
+  AuthDataSourceImpl(this.superbaseClient);
+
   @override
   Future<UserModel> getCurrentUserData() {
-    // TODO: implement getCurrentUserData
     throw UnimplementedError();
   }
 
   @override
   Future<UserModel> signInWithEmailPassword(
       {required String email, required String password}) {
-    // TODO: implement signInWithEmailPassword
     throw UnimplementedError();
   }
 
@@ -33,8 +37,23 @@ class AuthDataSourceImpl implements AuthDataSource {
   Future<UserModel> signUpWithEmailPassword(
       {required String username,
       required String email,
-      required String password}) {
-    // TODO: implement signUpWithEmailPassword
-    throw UnimplementedError();
+      required String password}) async {
+    try {
+      final res = await superbaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {
+          'username': username,
+        },
+      );
+      if (res.user != null) {
+        throw const ServerException("User already exists");
+      }
+      return UserModel.fromJson(res.user!.toJson());
+    } on AuthException catch (e) {
+      throw ServerException(e.message);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
   }
 }
