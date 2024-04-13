4a74d87cc6be900275f5213d89db0a57545f9bd7
gauss2302 <shilov6865@gmail.com>
2024-04-14 01:02:38 +0500

    Add AuthRepo interface for authentication feature

---
+++ lib/features/auth/domain/repositories/auth_repo.dart
@@ -0,0 +1,15 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:nail_it/core/error/failure.dart';
+import 'package:nail_it/features/auth/domain/entities/user_entities.dart';
+
+abstract interface class AuthRepo {
+  Future<Either<Failure, UserEntities>> signUpWithEmailPassword({
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, UserEntities>> signInWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
